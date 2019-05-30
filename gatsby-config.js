9b4e017e6e83ba8ea4bf0474e6f9bed9df334022
ipon <vazari197320@gmail.com>
2019-05-30 13:31:57 +0300

    fix:added new plugins in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,84 @@
+const autoprefixer = require('autoprefixer')
+const browserslist = require('browserslist')
+
+require('dotenv').config({
+    path: `.env.${ process.env.NODE_ENV }`,
+})
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `GatsbyBlog`,
+        description: `My first individual project on Gatsby`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${ __dirname }/src/assets/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `content`,
+                path: `${ __dirname }/src/content`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `partial`,
+                path: `${ __dirname }/src/partial`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-catch-links`,
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                includePaths: ['src/styles'],
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-relative-images`,
+                        options: {
+                            name: 'images',
+                        }
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1224,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-postcss`,
+            options: {
+                postCssPlugins: autoprefixer({ browsers: browserslist() }),
+            },
+        },
+
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `BluePes`,
+                short_name: `BluePes`,
+                start_url: `/`,
+                background_color: `#FFFFFF`,
+                theme_color: `#FFFFFF`,
+                display: `minimal-ui`,
+                icon: `src/assets/favicon.png`,
+            },
+        },
+    ]
 }
