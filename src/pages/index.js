163deb8ae534015dfb6694b56afc8ba5c1ea45d0
ipon <vazari197320@gmail.com>
2019-07-05 09:58:02 +0300

    new commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import SEO from '../components/seo'
 import Layout from '../components/layout'
+import TopIndex from '../components/top-index'
 
 const IndexPage = () => (
   <Layout>
     <SEO
       title="First - test-task"
       keywords={[`First`, `company`, `learn`, `IT`]} />
-    <div style={{ width: '100%', height: '88vh' }}/>
-
+    <TopIndex/>
   </Layout>
 )
 
